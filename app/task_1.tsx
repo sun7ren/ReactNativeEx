c6cac97e5b57b4822de27f2a89571e8922b5e922
Arini Nur Azizah <155711234+sun7ren@users.noreply.github.com>
2025-07-18 09:38:50 +0700

    Update task_1.tsx

--- app/task_1.tsx
+++ app/task_1.tsx
@@ -5,8 +5,8 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 export default function Task_1() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{height: 100, flexDirection: 'row'}}>
-        <View style={{ backgroundColor: 'lightblue'}}>
+      <SafeAreaView style={{backgroundColor: '#ffff', flex: 1}}>
+        <View style={{height: 100, width: 100, flexDirection: 'column', backgroundColor: 'lightblue'}}>
           <Text style={{alignContent: "center", margin: 'auto', padding: 10}}>Hello World!</Text>
         </View>
       </SafeAreaView>
